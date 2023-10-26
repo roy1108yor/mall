2fd9b915e453b55d13423066c6400b87bc9ba84b
kalougata <kalougata@111.com>
2023-10-27 07:36:41 +0800

    安装mysql驱动

--- controller/admin/ums_admin_controller.go
+++ controller/admin/ums_admin_controller.go
@@ -1,12 +1,21 @@
 package adminctrl
 
-import adminsrv "github.com/kalougata/mall/service/admin"
+import (
+	"github.com/gofiber/fiber/v2"
+	adminsrv "github.com/kalougata/mall/service/admin"
+)
 
 type umsAdminController struct {
 	service adminsrv.UmsAdminService
 }
 
 type UmsAdminController interface {
+	UmsAdminLogin(c *fiber.Ctx) error
+}
+
+// UmsAdminLogin implements UmsAdminController.
+func (*umsAdminController) UmsAdminLogin(c *fiber.Ctx) error {
+	panic("unimplemented")
 }
 
 func NewUmsAdminController(service adminsrv.UmsAdminService) UmsAdminController {
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require github.com/gofiber/fiber/v2 v2.50.0
 require (
 	github.com/andybalholm/brotli v1.0.5 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
+	github.com/go-sql-driver/mysql v1.7.1 // indirect
 	github.com/goccy/go-json v0.8.1 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/google/subcommands v1.0.1 // indirect
