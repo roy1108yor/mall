29aabad57ac7c339d817e25aeed8ae518ba8babb
kalougata <kalougata@111.com>
2023-10-26 21:53:17 +0800

    自定义http包

--- pkg/http/http.go
+++ pkg/http/http.go
@@ -1,12 +1,41 @@
 package http
 
 import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/log"
 )
 
 func Run(app *fiber.App, addr string) {
-	if err := app.Listen(addr); err != nil {
-		log.Panic(err)
+
+	go func() {
+		if err := app.Listen(addr); err != nil {
+			log.Panic(err)
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shutdown the server with
+	// a timeout of 5 seconds.
+	quit := make(chan os.Signal, 1)
+	// kill (no param) default send syscall.SIGTERM
+	// kill -2 is syscall.SIGINT
+	// kill -9 is syscall.SIGKILL but can't be catch, so don't need add it
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Info("Shutting down server...")
+
+	// The context is used to inform the server it has 5 seconds to finish
+	// the request it is currently handling
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := app.ShutdownWithContext(ctx); err != nil {
+		log.Fatal("Server forced to shutdown: ", err)
 	}
+
+	log.Info("Server exiting")
 }
